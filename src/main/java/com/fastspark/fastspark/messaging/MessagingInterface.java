6cde1f441d53df2ef4dfe91fc974f78a5ff16867
GimhaniU <gimhanieuthpala@gmail.com>
2017-11-07 20:57:38 +0530

    add basic implementation

--- src/main/java/com/fastspark/fastspark/messaging/MessagingInterface.java
+++ src/main/java/com/fastspark/fastspark/messaging/MessagingInterface.java
@@ -1,13 +1,54 @@
 package com.fastspark.fastspark.messaging;
 
+import com.fastspark.fastspark.model.Client;
+import com.fastspark.fastspark.model.Node;
+
+import java.io.IOException;
+
 /**
  * Created by Buddhi on 11/1/2017.
  */
 public class MessagingInterface {
+    Client client;
 
-    public String HandleMessage(String message) {
 
+    public String HandleMessage(String message) throws IOException {
+        String[] messagePart = message.split(" ");
+        switch (messagePart[1]) {
+            case "REGOK":
+                //handle  response from bootstrap
+                System.out.println(message);
+                this.client.handleRegisterResponse(message);
+                break;
+            case "UNROK": // handle unregister response
+                break;
+            case "JOINOK": // join response message
+                break;
+            case "LEAVEOK": // leave response message
+                break;
+            case "SEROK": // search response message
+                break;
+            case "HEARTBEATOK": //haddle hearbeat ok
+                System.out.println(message);
+                this.client.handleHeartBeatResponse(message);
+                break;
+            case "HEARTBEAT":
+                System.out.println(message);
+                this.client.sendHeartBeatReply(message);
+                break;
+            //this.client.
+            case "FBM": //multicast message to find a node from a bucket
+                System.out.println(message);
+                this.client.findNodeFromBucketReply(Integer.parseInt(messagePart[2]),new Node(client.getIp(), client.getPort()));
+                break;
+            case "FBMOK": //reply to FBM
+                this.client.receiveReplyFindNodeFromBucket(message);
+                break;
+
+        }
 
         return message;
     }
+
+
 }
--- src/main/java/com/fastspark/fastspark/model/Client.java
+++ src/main/java/com/fastspark/fastspark/model/Client.java
@@ -1,7 +1,13 @@
 package com.fastspark.fastspark.model;
 
-import java.util.ArrayList;
-import java.util.Map;
+import java.io.IOException;
+import java.net.*;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Buddhi on 11/1/2017.
@@ -15,8 +21,26 @@ public class Client {
     private String username;
     private Map<Integer, Node> bucketTable;
     private Map<String, ArrayList<String>> fileDictionary;
+    private Timestamp timestamp;
     private ArrayList<String> myFileList;
     private ArrayList<Node> myNodeList;
+    private DatagramSocket datagramSocket;
+
+    public Client(int k, int myBucketId, String ip, int port, Map<String, ArrayList<String>> fileDictionary, ArrayList<String> myFileList, DatagramSocket datagramSocket) throws SocketException {
+        this.k = k; // get from main
+        this.myBucketId = myBucketId;
+        //this.status = "0";
+        this.ip = ip;
+        this.port = port;
+       // this.userName = username;
+        this.bucketTable = new HashMap<>();
+        this.fileDictionary = fileDictionary;
+        this.myFileList = myFileList;
+        this.myNodeList = new ArrayList<>();
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.datagramSocket = datagramSocket;
+    }
+
 
     public int getK() {
         return k;
@@ -89,4 +113,352 @@ public class Client {
     public void setMyNodeList(ArrayList<Node> myNodeList) {
         this.myNodeList = myNodeList;
     }
+
+    public void sendMessage(String msg) {
+        try {
+            System.out.println("Sending message: " + msg);
+
+            DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, InetAddress.getByName(this.ip), 55555);
+            datagramSocket.send(dp);
+
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void initialize() {
+        // Register With Bootstrap Server
+        String msg = " REG " + this.ip + " " + this.port ;
+        msg = "00" + Integer.toString(msg.length()) + msg;
+
+        sendMessage(msg);
+
+    }
+
+    // handles REGOK responses from BS
+    // length REGOK no_nodes IP_1 port_1 IP_2 port_2
+    public void handleRegisterResponse(String msg) throws IOException {
+        String[] arr = msg.split(" ");
+
+        // validate msg
+        if (!arr[1].equals("REGOK")) {
+            return;
+        }
+
+        switch (arr[2]) {
+            case "0":
+                System.out.println("You are the first node, registered successfully with BS!");
+                // change up the "status" to ready (1) ????
+                break;
+            case "1":
+                storeNode(arr[3], arr[4]);
+                // change up the "status" to ready (1) ????
+                break;
+            case "2":
+                storeNode(arr[3], arr[4]);
+                storeNode(arr[5], arr[6]);
+                // change up the "status" to ready (1) ????
+                break;
+            case "9999":
+                System.out.println("failed, there is some error in the command");
+                break;
+            case "9998":
+                System.out.println("failed, already registered! attempting unregister first");
+                break;
+            case "9997":
+                System.out.println("failed, registered to another user, try a different IP and port");
+                // TODO
+                break;
+            case "9996":
+                System.out.println("failed, can't register. BS full.");
+            default:
+                // store FIRST 2 nodes' details
+                storeNode(arr[3], arr[4]);
+                storeNode(arr[5], arr[6]);
+
+                // complete bucketTable
+                for (int i = 0; i < k; i++) {
+
+                    if (!bucketTable.containsKey(i)) {
+//                        findNodeFromBucket(i);   //handle exceptions
+                    }
+                }
+
+                // complete myNodeList
+                if (myNodeList.isEmpty()) {
+                    // findNodeFromBucket(myBucketId);
+                    // send message to that returned node to get it's myNodeList and then store
+                } else {
+                    // send message to that node to get it's myNodeList and then store
+                }
+
+                // change up the "status" to ready (1)
+                break;
+        }
+//        while (true) {
+//            System.out.println("");
+//            System.out.print("Input Next Command : ");
+//
+//            msg = scanner.nextLine();
+//            switch (msg) {
+//                case "DISPLAY FILES":
+//                    displayFiles();
+//                    break;
+//                case "DISPLAY TABLE":
+//                    displayRoutingTable();
+//                    break;
+//                case "SEARCH FILES":
+//                    searchFiles(msg);
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+
+        connectWithNodes();
+
+    }
+
+    private void storeNode(String ip, String port) {
+        Node newNode = new Node(ip, Integer.parseInt(port));
+        int bucketId = (ip + ":" + port).hashCode() % k;
+        if (bucketId == this.myBucketId) {
+            myNodeList.add(newNode);
+        } else {
+            bucketTable.put(bucketId, newNode);
+        }
+    }
+
+    private void connectWithNodes() {
+        // indicate that I'm new to net
+        // send file list with this
+        // that node response with its myNodeList
+    }
+
+    public void connectWithNodesResponse(){
+
+    }
+
+    public void displayFiles() {
+        System.out.println("Files in ");
+        this.myFileList.forEach((file) -> {
+            System.out.println(file);
+        });
+    }
+
+    public void displayRoutingTable() {
+        if (myNodeList.isEmpty() && bucketTable.isEmpty()) {
+            System.out.println("Tables are empty");
+        } else {
+            System.out.println("Nodes list in the Bucket:");
+            for (Node node : myNodeList) {
+                System.out.println("\t" + node.getIp() + ":" + node.getPort());
+            }
+
+            System.out.println("Nodes list from other Buckets:");
+            Iterator entries = bucketTable.entrySet().iterator();
+            while (entries.hasNext()) {
+                Map.Entry entry = (Map.Entry) entries.next();
+                Integer key = (Integer) entry.getKey();
+                Node node = (Node) entry.getValue();
+                System.out.println("Bucket " + key + " : " + node.getIp() + ":" + node.getPort());
+            }
+        }
+    }
+
+    public void initializeSearch(String msg) throws IOException{
+        //SEARCH_FILES file_name
+        String file_name= msg.split(" ")[1];
+        String result_string="";
+
+        //length SEROK no_files IP port hops filename1 filename2 ... ...
+        ArrayList<String> results = new ArrayList<String>();
+        Pattern p = Pattern.compile(".*\\\\b"+file_name+"\\\\b.*");
+        Set<String> keys = fileDictionary.keySet();
+        Iterator<String> iterator = keys.iterator();
+
+        while (iterator.hasNext()) {
+            String candidate = iterator.next();
+            Matcher m = p.matcher(candidate);
+            if (m.matches()) {
+                results.add(candidate);
+                result_string.concat(candidate+" ");
+            }
+        }
+        System.out.println(result_string);
+
+        /////////
+        String net_message="SER "+this.getIp()+" "+this.getPort()+" "+msg.split(" ")[1]+" 1";
+        net_message = String.format("%04d", net_message.length() + 5) + " " + net_message;
+        searchFiles(net_message);
+    }
+
+    public void searchFiles(String message) throws UnknownHostException, IOException {
+        //length SER IP port file_name hops
+        String[] split = message.split(" ");
+        String file_name= split[4];
+        String result_string="";
+
+        int hop_count=0;
+        if(split.length==6)
+            hop_count=Integer.valueOf(split[5]);
+
+        //length SEROK no_files IP port hops filename1 filename2 ... ...
+        ArrayList<String> results = new ArrayList<String>();
+        Pattern p = Pattern.compile("[a-zA-Z\\s]*"+file_name+"[a-zA-Z\\s]*");
+
+        Set<String> keys = new HashSet<>(myFileList);
+        Iterator<String> iterator = keys.iterator();
+
+        ArrayList<String> nodes=new ArrayList<>();
+        ArrayList<Node> nodelist = new ArrayList<>();
+
+        //search in my files list
+        while (iterator.hasNext()) {
+            String candidate = iterator.next();
+            Matcher m = p.matcher(candidate);
+            if (m.matches()) {
+                results.add(candidate);
+                System.out.println(candidate);
+                result_string  = result_string.concat(candidate+" ");
+            }
+        }
+        if(results.size()>0){
+            String ret_message= "SEROK "+results.size()+" "+this.getIp()+" "+this.getPort()+" "+(hop_count++)+" "+result_string;
+            ret_message = String.format("%04d", ret_message.length() + 5) + " " + ret_message;
+            System.out.println(ret_message);
+        }else{
+
+            keys = fileDictionary.keySet();
+            iterator = keys.iterator();
+
+            boolean found=false;
+
+            while (iterator.hasNext()) {
+                String candidate = iterator.next();
+                Matcher m = p.matcher(candidate);
+                if (m.matches())
+                {
+                    nodes=fileDictionary.get(candidate);
+                    for(String node: nodes){
+                        nodelist.add(new Node(node.split(":")[0], Integer.parseInt(node.split(":")[1])));
+                    }
+                    multicast(message,nodelist );
+                }
+                found= true;
+            }
+
+            if(!found){
+                multicast(message, new ArrayList<Node>(bucketTable.values()));
+            }
+        }
+    }
+
+    public void findNodeFromBucket(int bucketId) throws UnknownHostException, IOException {
+        //FBM: Find Bucket Member 0011 FBM 01
+        String message = "FBM " + bucketId;
+        message = String.format("%04d", message.length() + 5) + " " + message;
+        multicast(message, myNodeList);
+    }
+
+    public void findNodeFromBucketReply(int bucketId, Node fromNode) throws UnknownHostException, IOException {
+        //FBMOK: Find Bucket Member OK
+        Node nodeFromBucket = null;
+        String message = null;
+        if (bucketTable.get(bucketId) != null) {
+            nodeFromBucket = bucketTable.get(bucketId);
+            message = "FBMOK " + bucketId + "" + nodeFromBucket.getIp() + " " + nodeFromBucket.getPort();
+        } else {
+            message = "FBMOK " + bucketId + " null null";
+        }
+        message = String.format("%04d", message.length() + 5) + " " + message;
+        unicast(message, fromNode);
+    }
+
+    public void receiveReplyFindNodeFromBucket(String message) throws UnknownHostException, IOException {
+
+        String[] split_msg = message.split(" ");
+        Node bucket_node = new Node(split_msg[3], Integer.valueOf(split_msg[4]));
+        if(this.getBucketTable().get(split_msg[2])!=null){
+            this.bucketTable.put(Integer.valueOf(split_msg[2]), bucket_node);
+        }
+    }
+
+    public void multicast(String message, ArrayList<Node> nodesList) throws SocketException, UnknownHostException, IOException {
+        //
+    }
+
+    public void unicast(String message, Node node) throws SocketException, UnknownHostException, IOException {
+        //
+    }
+
+    //gracefull leave
+    public void leave(int bucketId) throws IOException {
+        String message = "LEAVING BUCKET " + bucketId;
+        multicast(message, myNodeList);
+    }
+
+    public void updateRountingTable() throws IOException {
+        ArrayList<Node> temNodeList = new ArrayList<Node>();
+        for (Node node : myNodeList) {
+            if (timestamp.getTime() - node.getTimeStamp() < 5000) {
+                temNodeList.add(node);
+            }
+        }
+        this.myNodeList = temNodeList;
+
+        for (int key : bucketTable.keySet()) {
+
+            Node neighbour = bucketTable.get(key);
+            if (timestamp.getTime() - neighbour.getTimeStamp() > 5000) {
+                bucketTable.remove(key);
+                this.findNodeFromBucket(key);
+            }
+        }
+
+    }
+
+    public void handleHeartBeatResponse(String message) {
+        //length HEARTBEATOK IP_address port_no
+        boolean is_Change = false;
+        ArrayList<Node> temNodeList = new ArrayList<Node>();
+        String[] splitMessage = message.split(" ");
+        String ip = splitMessage[2];
+        int port = Integer.parseInt(splitMessage[3]);
+        for (Node node : myNodeList) {
+            if (node.getIp().equals(ip) && node.getPort() == port) {
+                node.setTimeStamp(timestamp.getTime());
+                is_Change = true;
+            }
+            temNodeList.add(node);
+        }
+        this.myNodeList = temNodeList;
+
+        if (!is_Change) {
+            for (int key : bucketTable.keySet()) {
+                Node node = bucketTable.get(key);
+                if (node.getIp().equals(ip) && node.getPort() == port) {
+                    node.setTimeStamp(timestamp.getTime());
+                    bucketTable.replace(key, node);
+                }
+            }
+        }
+    }
+
+    public void sendHeartBeatReply(String message) throws IOException {
+        String newMessage="HEARTBEATOK "+this.getIp()+" "+this.getPort();
+        newMessage = String.format("%04d", newMessage.length() + 5) + " " + newMessage;
+        String[] splitMessage = message.split(" ");
+        Node node = new Node(splitMessage[2], Integer.parseInt(splitMessage[3]));
+        unicast(newMessage, node);
+    }
+
+    void sendMessageToAPI(String message) {
+        //call to REST api calls
+    }
+
 }
