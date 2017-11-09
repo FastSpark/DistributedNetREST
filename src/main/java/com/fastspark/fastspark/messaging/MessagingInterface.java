9ff716a588002a338a62ef78796223815ee9e605
buddhiv <92buddhiv@gmail.com>
2017-11-09 22:22:41 +0530

    adding m interface

--- src/main/java/com/fastspark/fastspark/messaging/MessagingInterface.java
+++ src/main/java/com/fastspark/fastspark/messaging/MessagingInterface.java
@@ -1,12 +1,26 @@
 package com.fastspark.fastspark.messaging;
 
+import com.fastspark.fastspark.model.Client;
+import com.fastspark.fastspark.model.Node;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
 /**
  * Created by Buddhi on 11/1/2017.
  */
+@Component
 public class MessagingInterface {
+    Client client;
 
+<<<<<<< HEAD
     //handle message function
     public String HandleMessage(String message) {
         return message;
     }
 }
+=======
+
+
+}
+>>>>>>> dd8bd9eb6b990879a78ca2c64f13e60a86beeaab
